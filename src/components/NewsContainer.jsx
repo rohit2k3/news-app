d671ddb99ab07604551a50b431ce4b195ceffb7f
rohit2k3 <rohitsharma2k3@gmail.com>
2024-05-08 16:41:00 +0530

    first commit

--- src/components/NewsContainer.jsx
+++ src/components/NewsContainer.jsx
@@ -1,9 +1,19 @@
-import React,{useState} from 'react';
+import axios from 'axios';
+import React,{useEffect, useState} from 'react';
 
 
-const NewsContainer = () => {
+const NewsContainer = async () => {
     //api configuration
     const [article, setArticle] = useState([]);
+    useEffect(() => {
+      getArticles()
+    }, [])
+
+    const getArticles = async () => {
+      const getArticles = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=d23034a1e92640a9989a65109fb3ae4c`)
+      setArticle(getArticles.data.articles)
+    }
+    
   return (
     <div>
       <h1>sefs</h1>
